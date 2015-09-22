4be030c8837309c69e66df241ce4bc63f958a0f3
Jongmin Kim <kjm141215@gmail.com>
2015-09-22 22:41:24 +0100

    Can add two tags

--- app/app.rb
+++ app/app.rb
@@ -9,8 +9,9 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.create(title: params[:title], url: params[:url])
-    tag = Tag.create(name: params[:tag])
-    link.tags << tag
+    strings = params[:tag].split(/\s+/)
+    tags = strings.map { |tag| Tag.create(name: tag) }
+    tags.each { |tag| link.tags << tag }
     link.save
     redirect to '/links'
   end
