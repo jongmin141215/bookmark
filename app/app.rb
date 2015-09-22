df6bf3f179bf401c74f93febd186865c56baf995
Jongmin Kim <kjm141215@gmail.com>
2015-09-22 21:32:37 +0100

    Add tag model

--- app/app.rb
+++ app/app.rb
@@ -8,7 +8,10 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(title: params[:title], url: params[:url])
+    link = Link.create(title: params[:title], url: params[:url])
+    tag = Tag.create(name: params[:tag])
+    link.tags << tag
+    link.save
     redirect to '/links'
   end
 
@@ -16,4 +19,9 @@ class BookmarkManager < Sinatra::Base
     erb :'links/new'
   end
 
+  get '/tags/:tag' do
+    tag = Tag.first(name: params[:tag])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
 end
