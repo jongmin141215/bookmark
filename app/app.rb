0a5067f13ef72ccc99422d5176a41cddc04651a1
Jongmin Kim <kjm141215@gmail.com>
2015-09-23 17:42:35 +0100

    Improve UX: user does not need to type email again when submitting invalid form

--- app/app.rb
+++ app/app.rb
@@ -38,21 +38,23 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.new(email: params[:email],
+    @user = User.new(email: params[:email],
                     password: params[:password],
                     password_confirmation: params[:password_confirmation])
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect to '/links'
     else
       flash.now[:notice] = 'Password and confirmation password do not match'
       erb :'users/new'
     end
   end
+
 
 
 
